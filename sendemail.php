cabea2e002daeb6a1a9c8847a32d8b91d6c85c66
Jamie Zhang <jmzhang003@gmail.com>
2016-02-05 16:37:48 -0800

    Deleted more unnecessary things, made more changes to content of pages

--- sendemail.php
+++ sendemail.php
@@ -11,7 +11,7 @@
     $message = @trim(stripslashes($_POST['message'])); 
 
     $email_from = $email;
-    $email_to = 'email@gmail.com';
+    $email_to = 'cab.mailinglist@gmail.com';
 
     $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
