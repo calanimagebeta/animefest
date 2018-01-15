41b32a955b5f78c1005a2c31cb43a1045bc6b336
Izabela Tolentino <izabela.tolentino@gmail.com>
2018-01-14 18:25:40 -0800

    Added sponsors list

--- main.js
+++ main.js
@@ -10,5 +10,22 @@
 
     //Highlights top of nav when scrolling
     $('body').scrollspy({target: '#navbar'});
+    
+    //For hovering over sponsor list
+    var __sf = $(".sponsors ul span");
+    var __sf_cont = $(".sponsors ul");
+    var __title = $("#sponsor-title");
+
+    __sf.mouseenter(function() {
+      $(this).addClass('hover');
+      __sf_cont.addClass('enter');
+      var __setTitle = $(this).attr("data-group-title");
+      __title.html(__setTitle);
+    }).mouseleave(function() {
+      $(this).removeClass('hover');
+      __sf_cont.removeClass('enter');
+      var __setTitle = $(__title).attr("data-default-title");
+      __title.html(__setTitle);
+});
 
 })(jQuery);
